e149f761f3e00802fa454cbc2ddb5c8973fc9e74
Ale Sarco <ale@sarco.com.ar>
2020-12-23 01:37:23 +0000

    Fixed regex

--- AAesAmerican/src/main/java/com/sarcobjects/AmericanListener.java
+++ AAesAmerican/src/main/java/com/sarcobjects/AmericanListener.java
@@ -32,7 +32,7 @@ class AmericanListener extends AbstractStatusListener {
             //Do not respond to my own tweets
             if (!MY_USER_NAME.equalsIgnoreCase(status.getUser().getScreenName()) && !status.isRetweet()) {
                 String text = status.getText();
-                if (!text.matches("AA(?i)")) {  //reply only if AA is in the main tweet
+                if (!text.matches("(.*)AA(.*)(?i)")) {  //reply only if AA is in the main tweet
                     return;
                 }
                 if (nonNull(status.getQuotedStatus())) {
